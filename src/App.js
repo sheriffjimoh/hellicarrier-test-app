29bf59b79a60f3787cd0ca69fb6062de6457de1c
sheriffjimoh <sheriffjimoh88@gmail.com>
2022-07-09 12:59:57 -0700

    initial commit with table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,128 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
 
 function App() {
+
+
+  const [getProduct, setProduct] = useState([]);
+  const [getrawProduct, setrawProduct] = useState([]);
+  const [isLoading, setIsloading] = useState(false)
+
+function getAllFruits(){
+  setIsloading(true)
+
+  fetch(`http://fakestoreapi.com/products`)
+  .then((response) => response.json())
+  .then((data) => {
+    setIsloading(false)
+
+    if(data){
+      setrawProduct(data)
+        // make a fresh array with the categories
+      const groups = data?.reduce((groups, product) => {
+              const category = product.category;
+              if (!groups[category]) {
+                groups[category] = [];
+              }
+              groups[category].push(product);
+              return groups;
+            }, {});
+
+
+            // convert the array to object with accesible keys
+            const productGroups = Object.keys(groups).map((category) => {
+              return {
+                category,
+                products: groups[category]
+              };
+            });
+
+            setProduct(productGroups)
+           }  
+          })
+            .catch((err) => {
+              setIsloading(false)
+            console.log(err.message);
+            });
+}
+
+useEffect(() =>{
+ getAllFruits()
+},[])
+
+
+
+function handleSearch(e) {
+
+  var results = [];
+ var toSearch = e;
+  
+        // for(var i=0; i < getrawProduct.length; i++) {
+        //   for(var key in getrawProduct[i]) {
+        //     if(getrawProduct[i][key].indexOf(toSearch)!=-1) {
+        //       results.push(getrawProduct[i]);
+        //     }
+        //   }
+        // }
+
+    console.log("result::", getrawProduct)
+}
+
+
+function DiplayProduct(products) {
+   return  products.map((item, index)=>(
+                        
+    <div key={index}  className='tableContent'>
+          <span>{item.id}</span>
+          <span style={{width:100}}>{item.title}</span>
+          <span>{item.price}</span>
+          <span>{item.rating.rate}</span>
+          <img src={item.image} width="60" height="60" />
+     </div>
+ ) )
+}
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      
+          <div className='miniContainer'>
+
+              {/* search */}
+            
+              <div className='search-container'>
+                  <input type="search" onChange={(e) => handleSearch(e.target.value)}    placeholder='Search  with any keywords' className='search-input' />
+              </div>
+
+
+                     <header className='headerContainer'>
+                           <span>ID</span>
+                           <span>Product name</span>
+                           <span>Price</span>
+                           <span>Rating</span>
+                           <span>Logo</span>
+                     </header>
+
+                    {!isLoading ? getProduct.map((item,index) =>
+                         
+                      (
+                        < >
+                           <div key={index} className='category-label'>{item.category}</div>
+                           { DiplayProduct(item.products) }
+                        </>
+                       
+                       ) ) :
+                        <div className='containerCenter'>
+                                      <span>Loading...</span>
+                           </div>
+                      }
+                           
+                          
+
+                     
+            </div>
+     
     </div>
   );
 }
